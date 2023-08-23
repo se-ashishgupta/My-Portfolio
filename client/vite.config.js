fc5089458db85e10d4099c4ac5caa3667dbe603e
Ashish Gupta <gashish4950@gmail.com>
2023-08-23 12:02:36 +0530

    initial

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://creativeprogrammer2.onrender.com",
+  base: "https://creativeprogrammer.onrender.com",
 });
