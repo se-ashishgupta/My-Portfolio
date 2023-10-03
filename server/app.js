a0295e3e8835c0e626077f34dead50bb390706ae
Ashish Gupta <gashish4950@gmail.com>
2023-10-03 15:00:30 +0530

    added cors origin

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL1],
     method: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
